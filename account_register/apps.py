891badf02cf21d5f25db2e470f6877705c75f8e6
g4n3sha5 <kmatysiak-it@outlook.com>
2023-05-12 20:43:52 +0200

    whitenoise

--- account_register/apps.py
+++ account_register/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AccountRegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account_register'
+    name = 'account_register2'
 
 
 class UsersConfig(AppConfig):
